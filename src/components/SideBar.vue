a0ee8f203f4d0bf8f48f92a78509e2a04c7cdaee
adeola-addison <adeolaaddison@gmail.com>
2021-04-18 18:26:44 +0100

    Rating app

---
+++ src/components/SideBar.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="side-bar">
+    <div class="logo-sec">
+        <img class="logo" src="../assets/logo.png"  />
+
+        <span> FEEDBACK APP</span>
+    </div>
+    <div class="user-sec">
+
+       <img  class="round" src="../assets/user.jpeg" /> 
+
+       <span> Adeola Daniel</span> 
+    </div>
+   
+   <div class="team">
+      <h7>
+        Your Team mates
+      </h7>
+
+      <div class="user-sec" v-for="(user, ind) in users" :key="ind">
+
+       <img  class="round" v-bind:src="require(`@/assets/${user.img}`) || '' " /> 
+
+       <span>{{user.name}}</span> 
+    </div>
+     <!-- <div class="user-sec">
+
+       <img  class="round" src="../assets/user1.jpeg" /> 
+
+       <span> Maya Stones </span> 
+    </div> <div class="user-sec">
+
+       <img  class="round" src="../assets/user2.jpeg" /> 
+
+       <span> Jet Philips</span> 
+    </div> 
+    <div class="user-sec">
+
+       <img  class="round" src="../assets/user3.jpeg" /> 
+
+       <span> Sandra Miller</span> 
+    </div> -->
+   </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+  props: {
+    users: Array
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+.side-bar{
+
+       overflow-y: auto;
+    width: 30%;
+    background: black;
+    height: 100vh;
+    // width: 250;
+    padding: 0rem 0rem 0rem 2rem;
+    position: fixed;
+    top: 0;
+    
+}
+ .logo{
+
+   width: 13%;
+    
+ }
+ .logo-sec{
+       padding-top: 4rem ;
+       padding-bottom: 2rem ;
+
+   span{
+    
+     color: white;
+     font-weight: bolder;
+     padding: 1rem;
+     font-size: 1.3rem;
+    
+
+   }
+ }
+
+.user-sec{
+     margin-top: 2rem;
+}
+
+span{
+
+  color:#ffffffc2;
+  padding: 1rem;
+
+}
+
+.round{
+  
+    width: 50px;
+    height: 50px;
+    border-radius: 9rem;
+     object-fit: cover;
+  
+
+}
+
+.team{
+padding-top: 2rem;
+
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,124 @@
+<template>
+<div>
+ <div class="user-info">
+    <div class="user-desc">
+         <img src="../assets/user.jpeg"/>
+         <h5>Adeola Daniel</h5>
+         <p>Front-end Developer</p>
+    </div>
+ <h1 class="rated">Rating Summary</h1>
+        <div class="rate">
+            <input type="radio" id="star5" name="rate" value="7" />
+    <label for="star5" title="text">5 stars</label>
+            <input type="radio" id="star5" name="rate" value="6" />
+    <label for="star5" title="text">5 stars</label>
+    <input type="radio" id="star5" name="rate" value="5" />
+    <label for="star5" title="text">5 stars</label>
+    <input type="radio" id="star4" name="rate" value="4" />
+    <label for="star4" title="text">4 stars</label>
+    <input type="radio" id="star3" name="rate" value="3" />
+    <label for="star3" title="text">3 stars</label>
+    <input type="radio" id="star2" name="rate" value="2" />
+    <label for="star2" title="text">2 stars</label>
+    <input type="radio" id="star1" name="rate" value="1" />
+    <label for="star1" title="text">1 star</label>
+  </div>
+ 
+ </div>
+ <Rating/>
+
+
+ <SideBar v-bind:users="users"/>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SideBar from '@/components/SideBar.vue'
+import users from './user.json'
+
+
+
+
+export default {
+  name: 'Home',
+  components: {
+    SideBar,
+    
+  },
+
+  data(){
+     return{
+        users
+     }
+  },
+  mounted(){
+     console.log(typeof(users));
+  }
+}
+</script>
+
+<style lang="scss">
+.user-info{
+       margin-left: 30%;
+    padding: 2rem 2rem;
+}
+
+.user-desc{
+   margin-left: auto;
+   margin-right: auto;
+   img{
+           width: 20%;
+    border-radius: 7rem;
+    height: 10rem;
+    object-fit: cover;
+   }
+   h5{
+          padding: 0rem 2rem;
+   }
+   p{
+          padding: 0rem 2rem;
+   }
+
+}
+.rated{
+   color: #0299f6;
+}
+
+.rate {
+    float: left;
+    height: 46px;
+    padding: 0 10px;
+}
+.rate:not(:checked) > input {
+    position:absolute;
+    top:-9999px;
+}
+.rate:not(:checked) > label {
+    float:right;
+    width:1em;
+    overflow:hidden;
+    white-space:nowrap;
+    cursor:pointer;
+    font-size:30px;
+    color:#ccc;
+}
+.rate:not(:checked) > label:before {
+    content: '★ ';
+}
+.rate > input:checked ~ label {
+    color: #ffc700;    
+}
+.rate:not(:checked) > label:hover,
+.rate:not(:checked) > label:hover ~ label {
+    color: #deb217;  
+}
+.rate > input:checked + label:hover,
+.rate > input:checked + label:hover ~ label,
+.rate > input:checked ~ label:hover,
+.rate > input:checked ~ label:hover ~ label,
+.rate > label:hover ~ input:checked ~ label {
+    color: #c59b08;
+}
+
+</style>
